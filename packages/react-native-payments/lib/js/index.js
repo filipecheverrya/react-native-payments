bb663cfe0e8e23ae70dec0e7f570a903daf81675
Rodrigo Haertel <rodrigo.haertel@nodo.cc>
2020-02-10 17:07:49 -0300

    FIX: googlepay import from native modules

--- packages/react-native-payments/lib/js/index.js
+++ packages/react-native-payments/lib/js/index.js
@@ -1,9 +1,9 @@
 // @flow
 import { NativeModules, Platform } from 'react-native';
 
-const { RNGooglePay } = NativeModules;
+const { RNPayments } = NativeModules;
 import _PaymentRequest from './PaymentRequest';
 import { PKPaymentButton } from './PKPaymentButton';
 
 export const ApplePayButton = PKPaymentButton;
-export const PaymentRequest = Platform.OS === 'android' ? RNGooglePay : _PaymentRequest;
+export const PaymentRequest = Platform.OS === 'android' ? RNPayments : _PaymentRequest;
