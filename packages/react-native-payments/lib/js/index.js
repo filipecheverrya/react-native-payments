b5b2e2c41b87fb0b73903e07b9c8c489be1d9b70
Rodrigo Haertel <rodrigo.haertel@nodo.cc>
2020-02-10 16:48:33 -0300

    FEAT: add google pay support

--- packages/react-native-payments/lib/js/index.js
+++ packages/react-native-payments/lib/js/index.js
@@ -1,7 +1,9 @@
 // @flow
+import { NativeModules, Platform } from 'react-native';
 
+const { RNGooglePay } = NativeModules;
 import _PaymentRequest from './PaymentRequest';
 import { PKPaymentButton } from './PKPaymentButton';
 
 export const ApplePayButton = PKPaymentButton;
-export const PaymentRequest = _PaymentRequest;
+export const PaymentRequest = Platform.OS === 'android' ? RNGooglePay : _PaymentRequest;
